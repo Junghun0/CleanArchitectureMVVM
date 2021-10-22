11ba16f17af2bdaba61e302d03b0498aa18f14cf
parkjunghoon <jhpark0506@afreecatv.com>
2021-10-22 22:14:47 +0900

    test

--- app/src/main/java/com/example/cleanarchitecture/data/main/dto/PokemonResponse.kt
+++ app/src/main/java/com/example/cleanarchitecture/data/main/dto/PokemonResponse.kt
@@ -11,4 +11,7 @@ data class PokemonResponse(
     @SerializedName("next") val next: String?,
     @SerializedName("previous") val previous: String?,
     @SerializedName("results") val results: List<Pokemon>
+
+    //test1 push
+
 )
